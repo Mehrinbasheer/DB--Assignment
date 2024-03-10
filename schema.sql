8409fa0f127b767788bc9f9cd4974eeebc865a54
Mehrinbasheer <108331352+Mehrinbasheer@users.noreply.github.com>
2024-03-10 21:07:29 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,43 @@
+CREATE TABLE product_category (
+  id INT PRIMARY KEY,
+  name VARCHAR(255),
+  desc TEXT,
+  created_at TIMESTAMP,
+  modified_at TIMESTAMP,
+  deleted_at TIMESTAMP
+);
+
+CREATE TABLE product (
+  id INT PRIMARY KEY,
+  name VARCHAR(255),
+  desc TEXT,
+  SKU VARCHAR(255),
+  category_id INT,
+  inventory_id INT,
+  price DECIMAL(10, 2),
+  discount_id INT,
+  created_at TIMESTAMP,
+  modified_at TIMESTAMP,
+  deleted_at TIMESTAMP,
+  FOREIGN KEY (category_id) REFERENCES product_category(id)
+);
+
+CREATE TABLE product_inventory (
+  id INT PRIMARY KEY,
+  quantity INT,
+  created_at TIMESTAMP,
+  modified_at TIMESTAMP,
+  deleted_at TIMESTAMP
+);
+
+CREATE TABLE discount (
+  id INT PRIMARY KEY,
+  name VARCHAR(255),
+  desc TEXT,
+  discount_percent DECIMAL(10, 2),
+  active BOOLEAN,
+  created_at TIMESTAMP,
+  modified_at TIMESTAMP,
+  deleted_at TIMESTAMP
+);
+
